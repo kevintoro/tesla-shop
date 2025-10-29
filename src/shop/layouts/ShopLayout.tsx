59c2791eb3b104fe11235caeb967643df5e813d1
Kevin Toro <kevinandrestg7@gmail.com>
2025-10-28 22:58:36 -0500

    Feat: initial project and components

---
+++ src/shop/layouts/ShopLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import { CustomFooter } from "../components/CustomFooter";
+import { CustomHeader } from "../components/CustomHeader";
+
+export const ShopLayout = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <CustomHeader />
+      <Outlet />
+      <CustomFooter />
+    </div>
+  );
+};
---
+++ src/shop/pages/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+
+export const HomePage = () => {
+  return (
+    <>
+      <CustomJumbotron
+        title="Todos los productos"
+        subtitle="Ropa minimalista y elegante inspirada en el diseño futurista de Tesla. Calidad premium para un estilo atemporal."
+      />
+    </>
+  );
+};
